94fd0fc20f3eb263ebc5f4b291356aa355a63182
Quentin Loos <contact@quentinloos.be>
2016-03-15 18:37:54 +0100

    Activate application model

--- famoco/settings.py
+++ famoco/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'applications_mgmt.apps.ApplicationsMgmtConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
