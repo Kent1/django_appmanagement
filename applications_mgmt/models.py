3cebd99991f0dcf2defbdd7a6429af36dd776178
Quentin Loos <contact@quentinloos.be>
2016-03-15 18:33:56 +0100

    Add application model

--- applications_mgmt/models.py
+++ applications_mgmt/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Scope:
+
+    PUBLIC = 0
+    PRIVATE = 1
+
+    SCOPE = (
+        (PUBLIC, 'Public'),
+        (PRIVATE, 'Private'),
+    )
+
+class Application(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    scope = models.IntegerField(choices=Scope.SCOPE, default=Scope.PRIVATE)
+    application_file = models.FileField()
