d9e15c4f0487fd718cc76cc01e754173bfbdef37
Quentin Loos <contact@quentinloos.be>
2016-03-23 10:00:51 +0100

    Add the filtering with the scope

--- applications_mgmt/views.py
+++ applications_mgmt/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.core.urlresolvers import reverse_lazy
-from .models import Application
+from .models import Application, Scope
 
 class ApplicationCreateView(CreateView):
     model = Application
@@ -10,6 +10,15 @@ class ApplicationCreateView(CreateView):
 class ApplicationListView(ListView):
     model = Application
     fields = ['name', 'description', 'application_file', 'scope']
+
+    def get_queryset(self):
+        queryset = super(ListView, self).get_queryset()
+        scope = self.request.GET.get('scope')
+        if scope == 'public':
+            return Application.objects.filter(scope=Scope.PUBLIC)
+        elif scope == 'private':
+            return Application.objects.filter(scope=Scope.PRIVATE)
+        return queryset
 
 class ApplicationDetailView(DetailView):
     model = Application
